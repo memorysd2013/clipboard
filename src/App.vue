0aa72f112bf7dc981bfc2de997e224bf3503d2ea
Evan Ke <evan.ke@fusions360.com>
2024-12-29 16:42:24 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { Button, ConfigProvider, Cell, CellGroup } from 'vant';
+</script>
+
+<template lang="pug">
+ConfigProvider(theme="dark")
+  div app
+
+</template>
+
+<style scoped></style>
---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import './assets/main.css';
+import 'vant/lib/index.css';
+
+import { createApp } from 'vue';
+import App from './App.vue';
+
+const app = createApp(App);
+app.mount('#app');
