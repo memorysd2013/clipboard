7351a8b312e2591cdf1b0143f385218beba13627
Evan Ke <evan.ke@fusions360.com>
2025-01-19 06:37:03 +0800

    feat: add required dependency, global config adjustment

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import './assets/main.css';
 import 'vant/lib/index.css';
 
+import {
+  Popup,
+  Field,
+  Button,
+  CellGroup,
+  SwipeCell,
+  Cell,
+  Tabs,
+  Tab,
+  TextEllipsis,
+  Icon,
+} from 'vant';
 import { createApp } from 'vue';
 import App from './App.vue';
 
 const app = createApp(App);
+app.use(Popup);
+app.use(Field);
+app.use(CellGroup);
+app.use(Cell);
+app.use(SwipeCell);
+app.use(Tabs);
+app.use(Tab);
+app.use(TextEllipsis);
+app.use(Button);
+app.use(Icon);
 app.mount('#app');
